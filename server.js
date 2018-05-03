073475378bbac84be1b0081402f416a4c1f6a0fe
garr1s0n <gary.wilpizeski@gmail.com>
2018-05-03 12:34:30 -0400

    Adding Reg/Login Routes

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+function isEmpty(str) {
+    return !str || 0 === str.length;
+}
+
 app.get('/', function(req,res){
     res.send("Welcome to Invoicing App");
 });
@@ -17,11 +21,57 @@ app.listen(PORT, function(){
     console.log(`App running on localhost:${PORT}`);
 });
 
-app.post('/register', function(res, req) {
-    if( isEmpty(req.body.name)  || isEmpty(req.body.email) || isEmpty(req.body.company_name) || isEmpty(req.body.password) ){
+app.post('/register', function(req, res) {
+    if ( isEmpty(req.body.name) || isEmpty(req.body.email) || isEmpty(req.body.company_name) || isEmpty(req.body.password) ) {
         return res.json({
-            'status' : false,
-            'message' : 'All fields are required'
+            status: false,
+            message: "All fields are required"
         });
-    }
+      }
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        let db = new sqlite3.Database("./database/InvoicingApp.db");
+        let sql = `INSERT INTO users(name, email, company_name, password) VALUES('${req.body.name}', '${req.body.email}', '${req.body.company_name}', '${hash}')`;
+        db.run(sql, function(err) {
+            if(err) {
+                throw err;
+            } else {
+                return res.json({
+                    status: true,
+                    message: "User Created"
+                })
+            }
+        });
+        db.close();
+    });
+});
+
+app.post('/login', function(req, res) {
+    let db = new sqlite3.Database('./database/InvoicingApp.db');
+    let sql = `SELECT * from users where email='${req.body.email}'`;
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        db.close();
+        if (rows.length == 0) {
+            return res.json({
+                status: false,
+                message: "Sorry. Incorrect Email Address"
+            });
+        }
+        let user = rows[0];
+        let authenticated = bcrypt.compareSync(req.body.password, user.password);
+        delete user.password;
+        if (authenticated) {
+            return res.json({
+                status: true,
+                user: user
+            });
+        }
+        return res.json({
+            status: false,
+            message: "Sorry. Incorrect Password. Please retry."
+        });
+    });
 });
