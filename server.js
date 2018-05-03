84331b1710bada57225ef1084779f8489ef52e00
Gary W <gary.wilpizeski@gmail.com>
2018-05-03 08:42:51 -0400

    updating server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
 const PORT = process.env.PORT || 3128;
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
@@ -13,4 +15,13 @@ app.get('/', function(req,res){
 
 app.listen(PORT, function(){
     console.log(`App running on localhost:${PORT}`);
+});
+
+app.post('/register', function(res, req) {
+    if( isEmpty(req.body.name)  || isEmpty(req.body.email) || isEmpty(req.body.company_name) || isEmpty(req.body.password) ){
+        return res.json({
+            'status' : false,
+            'message' : 'All fields are required'
+        });
+    }
 });
